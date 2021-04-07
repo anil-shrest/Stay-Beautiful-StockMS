af8c73bc1c047e60fbf191b90d016be5d2f36dc5
Pratistha Kansakar <pratistha.k428@gmail.com>
2021-04-07 16:31:48 +0545

    Add Update and Delete row function to category grid view.

--- Category.aspx.cs
+++ Category.aspx.cs
@@ -65,5 +65,67 @@ namespace StayBeautifulSMS
 
             this.BindGrid();
         }
+
+        protected void categoryGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != categoryGridView.EditIndex)
+            {
+                (e.Row.Cells[0].Controls[2] as LinkButton).Attributes["onclick"] = "return confirm('Do you want to delete this row?');";
+
+            }
+        }
+
+        protected void categoryGridView_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            categoryGridView.EditIndex = e.NewEditIndex;
+            this.BindGrid();
+        }
+
+        protected void categoryGridView_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            categoryGridView.EditIndex = -1;
+            this.BindGrid();
+        }
+
+        protected void categoryGridView_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = categoryGridView.Rows[e.RowIndex];
+            int ID = Convert.ToInt32(categoryGridView.DataKeys[e.RowIndex].Values[0]);
+            string categoryName = (row.Cells[2].Controls[0] as TextBox).Text;
+            string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            using (OleDbConnection con = new OleDbConnection(constr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand("Update Category SET Category_Name = '" + categoryName + "' where category_id = " + ID))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+            categoryGridView.EditIndex = -1;
+            this.BindGrid();
+        }
+
+        protected void categoryGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int ID = Convert.ToInt32(categoryGridView.DataKeys[e.RowIndex].Values[0]);
+            string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (OleDbConnection con = new OleDbConnection(constr))
+            {
+                OleDbCommand cmd1 = new OleDbCommand("UPDATE Items SET category_id = NULL WHERE category_id = " + ID);
+                OleDbCommand cmd2 = new OleDbCommand("DELETE FROM Category WHERE category_id = " + ID);
+                cmd1.Connection = con;
+                cmd2.Connection = con;
+                con.Open();
+                cmd1.ExecuteNonQuery();
+                cmd2.ExecuteNonQuery();
+                con.Close();
+            }
+            this.BindGrid();
+        }
     }
 }
