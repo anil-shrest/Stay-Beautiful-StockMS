246f19007af076e146b26c7e64fd29369fb18b22
Sachhyam Shakya <sachhyamcrook@gmail.com>
2021-04-07 14:12:04 +0545

    Brand Form

---
+++ Brand.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StayBeautifulSMS
+{
+    public partial class Brand : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ////default data load
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+        private void BindGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            OleDbCommand cmd = new OleDbCommand();
+            OleDbConnection con = new OleDbConnection(constr);
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = @"SELECT Brand_Id, Brand_Name ""Brand Name"" FROM Brand";
+            //cmd.CommandText = "SELECT * FROM [customer]";
+            cmd.CommandType = CommandType.Text;
+            DataTable dt = new DataTable("customer");
+            using (OleDbDataReader sdr = cmd.ExecuteReader())
+            {
+                dt.Load(sdr);
+            }
+            con.Close();
+            brandGridView.DataSource = dt;
+            brandGridView.DataBind();
+        }
+    }
+  
+}
